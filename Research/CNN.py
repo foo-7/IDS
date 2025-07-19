10c9d0f1c3c70b61bd607b331b0d4fd2aa05e73c
foo-7 <derinoah004@gmail.com>
2025-07-19 00:30:33 -0700

    nn training and testing, need debugging later

--- Research/CNN.py
+++ Research/CNN.py
@@ -19,6 +19,8 @@ from sklearn.model_selection import train_test_split as TTS
 from DataPreprocess import DataPreprocess
 from Net import Net
 
+from torch.utils.data import TensorDataset, DataLoader
+
 # Verifying that GPU is being utilized
 print("Cuda available:", torch.cuda.is_available())
 print("Device name:", torch.cuda.get_device_name(0))
@@ -50,4 +52,69 @@ X_test_tensor = torch.tensor(X_test_scaled, dtype=torch.float32)
 y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32)
 y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32)
 
+
+Train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+Test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+
+Train_loader = DataLoader(Train_dataset, batch_size=64, shuffle=True)
+Test_loader = DataLoader(Test_dataset, batch_size=64, shuffle=True)
+
+n_epochs = 3
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
+
 network = Net()
+optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
+
+"""
+TRAINING MODEL
+"""
+
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(Train_loader.dataset) for i in range(n_epochs + 1)]
+
+def train(epoch):
+    network.train()
+    for batch_idx, (data, target) in enumerate(Train_loader):
+        optimizer.zero_grad()
+        output = network(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss:{:.6f}'.format(
+                epoch, batch_idx * len(data), len(Train_loader), 100. * batch_idx / len(Train_loader), loss.item()
+            ))
+            train_losses.append(loss.item())
+            train_counter.append((batch_idx * 64) + ((epoch - 1) * len(Train_loader.dataset)))
+
+"""
+EVALUATING MODEL PERFROMANCE
+"""
+def test():
+    network.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in Test_loader:
+            output = network(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).sum().item()
+            test_loss /= len(Test_loader.dataset)
+            test_losses.append(test_losses)
+            print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+                test_loss, correct, len(Test_loader.dataset), 100. * correct / len(Test_loader.dataset)
+            ))
+
+"""
+Running Training and Testing Loops
+"""
+test()
+for epoch in range(1, n_epochs + 1):
+    train(epoch)
+    test
