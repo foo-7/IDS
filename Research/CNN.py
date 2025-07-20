83d46da864ccd500539eab3f6b24585b1be43df2
foo-7 <derinoah004@gmail.com>
2025-07-20 14:34:05 -0700

    Added summary for accuracy for both jamming and spoofing files

--- Research/CNN.py
+++ Research/CNN.py
@@ -29,9 +29,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 """
     Data preprocessing
 """
-fileName1 = "jamming-merged-gps-only.csv"
-fileName2 = "spoofing-merged-gps-only.csv"
-DP = DataPreprocess(fileName2)
+fileName1 = "jamming-merged-gps-only.csv"  # Highest Accuracy: 90
+fileName2 = "spoofing-merged-gps-only.csv" # Highest Accuracy: 90.15%
+DP = DataPreprocess(fileName1)
 df = DP.run(givenTargets={'benign' : 0, 'malicious' : 1}, targetName='label')
 
 X = df.drop(columns='label')
